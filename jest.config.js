eea26b07bd5f9281c5b0602660a45229ea395492
Peter Jenkins <peter.jenkins@smartly.io>
2024-10-10 06:27:20 +0300

    cleanup jest config

--- jest.config.js
+++ jest.config.js
@@ -1,30 +1,9 @@
-    // "jest": {
-    //     "globals": {
-    //       "ts-jest": {
-    //         "diagnostics": false
-    //       }
-    //     },
-    //     "moduleFileExtensions": [
-    //       "ts",
-    //       "tsx",
-    //       "js"
-    //     ],
-    //     "transform": {
-    //       "^.+\\.(ts|tsx)$": "ts-jest"
-    //     },
-    //     "testMatch": [
-    //       "**/tests/*jest.+(ts|tsx|js)"
-    //     ]
-    //   },
 module.exports = {
-    globals: {
-        'ts-jest': {
-            diagnostics: false,
-        },
-    },
     moduleFileExtensions: ['ts', 'tsx', 'js'],
     transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest',
+        '^.+\\.(ts|tsx)$': [ 'ts-jest', {
+            diagnostics: false,
+        }],
     },
     testMatch: ['**/tests/*jest.+(ts|tsx|js)'],
 };
